18c5f029ec082a19cfd16cff83faeeebf93fcd00
rafarui <rafarui@gmail.com>
2020-10-02 20:05:40 -0300

    update 0.0.58

--- main.py
+++ main.py
@@ -38,18 +38,6 @@ parser.add_argument("--skip-consolidate",
 args = parser.parse_args()
 
 
-def update_settings(login):
-    app = Apps(login)
-    app_name = 'techfinplatform'
-    app_id = app.get_by_name(app_name)['mdmId']
-
-    data = [{"mdmName": "skip_pause", "mdmParameterValue": False},
-            {"mdmName": "clean_dm", "mdmParameterValue": True},
-            {"mdmName": "clean_etls", "mdmParameterValue": True}]
-
-    settings_id = login.call_api(path=f'v1/tenantApps/{app_id}/settings', )['mdmId']
-
-    _ = login.call_api(path=f'v1/tenantApps/{app_id}/settings/{settings_id}?publish=true', method='PUT', data=data)
 
 
 def get_login(domain, org):
@@ -64,51 +52,7 @@ def get_login(domain, org):
     return login
 
 
-def par_consolidate(login, staging_name, connector_name):
-    cds_stag = CDSStaging(login)
-    n_r = cds_stag.count(staging_name=staging_name, connector_name=connector_name)
-    if n_r > 5000000:
-        worker_type = 'n1-highmem-16'
-        max_number_workers = 16
-    else:
-        worker_type = 'n1-highmem-4'
-        max_number_workers = 16
-    number_shards = round(n_r / 100000) + 1
-    number_shards = max(16, number_shards)
-    task_id = cds_stag.consolidate(staging_name=staging_name, connector_name=connector_name, worker_type=worker_type,
-                                   number_shards=number_shards, rehash_ids=True, max_number_workers=max_number_workers)
-    return task_id
-
-
-def consolidate_stagings(login):
-    current_cell = sheet_utils.find_tenant(techfin_worksheet, login.domain)
-    #TODO: Need to rethink how to do this. If need tables are added need to change here.
-    main_tables = ['ar1', 'cko', 'company', 'ct1', 'ctl', 'ctt', 'currency', 'cv3', 'cvd', 'fk1',
-                   'fk2', 'fk5', 'fk7', 'fkc', 'fkd', 'frv', 'invoicexml', 'mapping', 'organization',
-                   'paymentstype', 'sa1', 'sa2', 'sa6', 'sd1', 'sd2', 'se1', 'se2', 'se8', 'sea', 'sf1', 'sf2',
-                   'sf4', 'protheus_sharing']
-
-    tasks = defaultdict(list)
-    task_id = Parallel(n_jobs=5, backend='threading')(delayed(par_consolidate)(login, staging_name=i,
-                                                                               connector_name='protheus_carol')
-                                                      for i in main_tables)
-    tasks[domain].extend(task_id)
-    task_list = [i['data']['mdmId'] for i in tasks[login.domain]]
-
-    try:
-        task_list, fail = track_tasks(login, task_list)
-    except:
-        fail = True
-
-    if fail:
-        logger.error(f"Problem with {login.domain} durring consolidate.")
-        sheet_utils.update_status(techfin_worksheet, current_cell.row, 'FAILED')
-        exit(1)
-
-    return task_list
-
-
-def track_tasks(login, task_list, do_not_retry=False):
+def track_tasks(login, task_list, do_not_retry=False, logger=None):
     retry_tasks = defaultdict(int)
     n_task = len(task_list)
     max_retries = set()
@@ -144,31 +88,7 @@ def track_tasks(login, task_list, do_not_retry=False):
             logger.debug('Waiting for tasks')
 
 
-def run_app(login, app_name, process_name, ):
-    current_cell = sheet_utils.find_tenant(techfin_worksheet, login.domain)
-
-    task_list = []
-    app = Apps(login)
-    app_id = app.get_by_name(app_name)['mdmId']
-
-    params = {"entitySpace": "WORKING", "checkAllSpaces": True}
-    process_id = login.call_api(f"v1/tenantApps/{app_id}/aiprocesses", method='GET', params=params)["mdmId"]
-    process_id = login.call_api(f"v1/tenantApps/{app_id}/aiprocesses/{process_id}/execute/{process_name}",
-                                method='POST')["data"]["mdmId"]
-
-    try:
-        task_list, fail = track_tasks(login, [process_id], do_not_retry=True)
-    except:
-        fail = True
-
-    if fail:
-        logger.error(f"Problem with {login.domain} during Processing.")
-        sheet_utils.update_status(techfin_worksheet, current_cell.row, 'FAILED')
-        exit(1)
-    return task_list
-
-
-def update_app(login, app_name, app_version):
+def update_app(login, app_name, app_version, logger):
     current_cell = sheet_utils.find_tenant(techfin_worksheet, login.domain)
 
     to_install = login.call_api("v1/tenantApps/subscribableCarolApps", method='GET')
@@ -179,8 +99,9 @@ def update_app(login, app_name, app_version):
         to_install_id = to_install['mdmId']
     else:
         logger.error("Error trying to update app")
-        sheet_utils.update_status(techfin_worksheet, current_cell.row, 'FAILED')
-        exit(1)
+        sheet_utils.update_status(techfin_worksheet, current_cell.row, 'FAILED - did not found app to install')
+        return []
+
     updated = login.call_api(f"v1/tenantApps/subscribe/carolApps/{to_install_id}", method='POST')
     params = {"publish": True, "connectorGroup": "protheus"}
     install_task = login.call_api(f"v1/tenantApps/{updated['mdmId']}/install", method='POST', params=params)
@@ -193,9 +114,29 @@ def update_app(login, app_name, app_version):
 
     if fail:
         logger.error(f"Problem with {login.domain} during App installation.")
-        sheet_utils.update_status(techfin_worksheet, current_cell.row, 'FAILED')
-        exit(1)
+        sheet_utils.update_status(techfin_worksheet, current_cell.row, 'FAILED - app install')
+        return []
+
     return task_list
+
+
+def par_processing(login, staging_name, connector_name, delete_realtime_records=False,
+                   delete_target_folder=False):
+    cds_stag = CDSStaging(login)
+    n_r = cds_stag.count(staging_name=staging_name, connector_name=connector_name)
+    if n_r > 5000000:
+        worker_type = 'n1-highmem-16'
+        max_number_workers = 16
+    else:
+        worker_type = 'n1-highmem-4'
+        max_number_workers = 16
+    number_shards = round(n_r / 100000) + 1
+    number_shards = max(16, number_shards)
+    task_id = cds_stag.process_data(staging_name=staging_name, connector_name=connector_name, worker_type=worker_type,
+                                    number_shards=number_shards, max_number_workers=max_number_workers,
+                                    delete_target_folder=delete_target_folder,
+                                    delete_realtime_records=delete_realtime_records)
+    return task_id
 
 
 def get_app_version(login, app_name, version):
@@ -204,16 +145,14 @@ def get_app_version(login, app_name, version):
     return app_info['mdmAppVersion']
 
 
-if __name__ == "__main__":
+def run(domain, org='totvstechfin'):
 
-    domain = args.tenant
-    org = args.org
-    skip_consolidate = args.skip_consolidate
-
+    # avoid all tasks starting at the same time.
+    time.sleep(round(1 + random.random() * 5, 2))
+    org = 'totvstechfin'
     app_name = "techfinplatform"
-    process_name = "processAll"
-    app_version = '0.0.54'
-
+    app_version = '0.0.58'
+    connector_name = 'protheus_carol'
     # Create slack handler
     slack_handler = SlackerLogHandler(os.environ["SLACK"], '#techfin-reprocess',  # "@rafael.rui",
                                       username='TechFinBot')
@@ -230,9 +169,11 @@ if __name__ == "__main__":
     login = get_login(domain, org)
 
     current_cell = sheet_utils.find_tenant(techfin_worksheet, login.domain)
-    if not current_cell:
-        logger.error(f"{login.domain} not found in the sheet")
-        raise ValueError
+    status = techfin_worksheet.row_values(current_cell.row)[-1]
+
+    if 'done' in status.strip().lower() or 'failed' in status.strip().lower():
+        logger.info(f"Nothing to do in {domain}")
+        return
 
     sheet_utils.update_start_time(techfin_worksheet, current_cell.row)
     logger.info(f"Starting process {domain}")
@@ -249,20 +190,56 @@ if __name__ == "__main__":
         logger.info(f"Running version {app_version}")
         sheet_utils.update_version(techfin_worksheet, current_cell.row, app_version)
 
-    # Update app params.
-    update_settings(login)
+    to_reprocess = [
+        'fk5_transferencia',
+        'fkd_1',
+        'se1_payments_abatimentos',
+        'fk1',
+        'se1_acresc_1',
+        'fk5_estorno_transferencia_pagamento',
+        'fkd_deletado',
+        'se1_decresc_1',
+        'se1_payments',
+        'sea_1_frv_descontado_deletado_invoicepayment',
+        'sea_1_frv_descontado_naodeletado_invoicepayment',
+    ]
 
-    # consolidate.
-    if not skip_consolidate:
-        sheet_utils.update_status(techfin_worksheet, current_cell.row, "Consolidating + appRunning")
-        logger.info(f"Staging consolidate {domain}")
-        _ = consolidate_stagings(login)
-        logger.info(f"Done consolidate {domain}")
-    sheet_utils.update_status(techfin_worksheet, current_cell.row, "Process")
-    logger.info(f"Starting app {domain}")
+    tasks_to_track = []
+    for i, staging_name in enumerate(to_reprocess):
+        if i == 0:
+            task = par_processing(login, staging_name, connector_name, delete_realtime_records=True,
+                                  delete_target_folder=True)
+            time.sleep(5) #time to delete RT.
+        else:
+            task = par_processing(login, staging_name, connector_name, delete_realtime_records=False,
+                                  delete_target_folder=False)
+        tasks_to_track.append(task['data']['mdmId'])
 
-    # run app.
-    _ = run_app(login, app_name=app_name, process_name=process_name)
+    try:
+        task_list, fail = track_tasks(login, tasks_to_track, logger=logger)
+    except:
+        fail = True
+
+    if fail:
+        logger.error(f"Problem with {login.domain} durring consolidate.")
+        sheet_utils.update_status(techfin_worksheet, current_cell.row, 'FAILED')
+        return
+
     logger.info(f"Finished all process {domain}")
     sheet_utils.update_status(techfin_worksheet, current_cell.row, "Done")
     sheet_utils.update_end_time(techfin_worksheet, current_cell.row)
+
+    return task_list
+
+
+if __name__ == "__main__":
+
+    table = techfin_worksheet.get_all_records()
+    table = [t['environmentName (tenantID)'] for t in table if t.get('environmentName (tenantID)', None) is not None]
+
+    import multiprocessing
+    pool = multiprocessing.Pool(6)
+    pool.map(run, table)
+    pool.close()
+    pool.join()
+
